e7766c690e9b4ea78953a89a784a1b09d2683dd0
Hidde Bultsma <hidde.bultsma@hva.nl>
2014-11-25 13:46:09 +0100

    Adds cache lifetime attribute to Wrap initializer and updates setup.py settings

--- tortilla/api.py
+++ tortilla/api.py
@@ -3,6 +3,6 @@
 from . import wrappers
 
 
-def wrap(url, debug=False):
+def wrap(url, debug=False, cache_lifetime=None):
     """Syntax sugar for creating service wrappers."""
-    return wrappers.Wrap(part=url, debug=debug)
+    return wrappers.Wrap(part=url, debug=debug, cache_lifetime=cache_lifetime)
